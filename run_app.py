6aaf8aa5030ba0565e06f26fe0a517dd8dd371ea
I-am-Fine0358 <oyafuso.genki@gmail.com>
2024-07-03 13:13:27 +0900

    CPについて完成

--- run_app.py
+++ run_app.py
@@ -3,23 +3,39 @@ import pandas as pd
 import os
 from data_obtain import CP_grapher
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from io import BytesIO
 
 # タイトル
-st.title('グラフプロッター')
-fig, ax = plt.subplots()
-st.session_state['general_plot_settings'] = {}
+st.title('複数データを管理するアプリ')
 
-# データフォルダのリストの初期化
+# セッションステートの初期化
 if 'data_folders' not in st.session_state:
     st.session_state['data_folders'] = []
-    st.session_state['plot_settings'] = []
-    st.session_state['line'] = []
+if 'data' not in st.session_state:
     st.session_state['data'] = []
-    st.session_state['linewidth'] = []
-    st.session_state['x_min_lim'] = 0
-    st.session_state['x_max_lim'] = 1
-    st.session_state['y_min_lim'] = 0
-    st.session_state['y_max_lim'] = 1
+if 'plot_colors' not in st.session_state:
+    st.session_state['plot_colors'] = []
+if 'legends' not in st.session_state:
+    st.session_state['legends'] = []
+if 'line_types' not in st.session_state:
+    st.session_state['line_types'] = []
+if 'line_widths' not in st.session_state:
+    st.session_state['line_widths'] = []
+if 'x_min' not in st.session_state:
+    st.session_state['x_min'] = 0.0
+if 'x_max' not in st.session_state:
+    st.session_state['x_max'] = 10.0
+if 'y_min' not in st.session_state:
+    st.session_state['y_min'] = 0.0
+if 'y_max' not in st.session_state:
+    st.session_state['y_max'] = 10.0
+
+# サイドバーにプロットモード選択ウィジェットを追加
+plot_mode = st.sidebar.selectbox('プロットモードを選択', ['matplot', 'plotly'])
+
+# サイドバーにCV / CPの選択欄を追加
+plot_type = st.sidebar.selectbox('プロットタイプを選択', ['CP', 'CV'])
 
 # サイドバーにフォルダ選択ウィジェットを追加
 folder = st.sidebar.text_input('データフォルダのパスを入力', '')
@@ -28,66 +44,127 @@ folder = st.sidebar.text_input('データフォルダのパスを入力', '')
 if st.sidebar.button('フォルダを追加'):
     if os.path.isdir(folder):
         st.session_state['data_folders'].append(folder)
-        st.session_state['plot_settings'].append({
-            'color': 'Black',  # デフォルトの色
-        })
-        st.session_state['line'].append({
-            'line_type': '-',
-        })
-        st.session_state['linewidth'].append({
-            'line_width': 3,
-        })
+        data = CP_grapher(folder)
+        st.session_state['data'].append(data)
+        st.session_state['plot_colors'].append('white')  # デフォルトの色を白に設定
+        st.session_state['legends'].append(os.path.basename(folder))  # デフォルトの凡例をフォルダ名に設定
+        st.session_state['line_types'].append('solid')  # デフォルトの線の種類を設定
+        st.session_state['line_widths'].append(2)  # デフォルトの線幅を設定
+        # 各軸の最小値と最大値を計算
+        st.session_state['x_min'] = min(st.session_state['x_min'], data["time/s"].min())
+        st.session_state['x_max'] = max(st.session_state['x_max'], data["time/s"].max())
+        st.session_state['y_min'] = min(st.session_state['y_min'], data["Ewe/V"].min())
+        st.session_state['y_max'] = max(st.session_state['y_max'], data["Ewe/V"].max())
     else:
         st.sidebar.error('無効なフォルダパスです。')
 
 # アップロードされたフォルダの一覧を表示
 st.sidebar.write('追加されたフォルダ:')
+preset_colors = ['white', 'blue', 'orange', 'green', 'red', 'violet', 'brown']
+preset_types = {
+    "solid": 'solid',
+    "dash": 'dash',
+    "dashdot": 'dashdot',
+    "dot": 'dot'
+}
+matplotlib_line_types = {
+    "solid": '-',
+    "dash": '--',
+    "dashdot": '-.',
+    "dot": ':'
+}
+
 for i, folder in enumerate(st.session_state['data_folders']):
-    # folderの最後のディレクトリ名を取得
-    folder_name = os.path.basename(folder)
-    st.sidebar.write(os.path.basename(folder))
-    
-    # プロット設定（色）をサイドバーで選択
-    preset_colors = ['black', 'blue', 'orange', 'green', 'red', 'violet', 'brown']
-    color_choice = st.sidebar.radio(f'フォルダ {i+1} の色を選択', preset_colors, index=0, key=f'color_choice_{i}', horizontal=True)
-    preset_types = ["-", "--", "-.", ":"]
-    line_choice = st.sidebar.radio(f'フォルダ {i+1} のラインタイプを選択', preset_types, index=0, key=f'line_choice_{i}', horizontal=True)
-    # linewidthをテキストボックスで選択
-    st.session_state['linewidth'][i]['line_width'] = st.sidebar.number_input('ライン幅を選択', min_value=1, max_value=10, value=3, key=f'line_width_{i}')
+    st.sidebar.write(f'フォルダ {i+1}: {os.path.basename(folder)}')
 
-    st.session_state['plot_settings'][i]['color'] = color_choice
-    st.session_state['line'][i]['line_type'] = line_choice
+    # 色をサイドバーで選択
+    color_choice = st.sidebar.selectbox(f'フォルダ {i+1} の色を選択', preset_colors, index=0, key=f'color_choice_{i}')
+    st.session_state['plot_colors'][i] = color_choice
 
-    data = CP_grapher(folder)
-    st.session_state['data'].append(data)
-    
-    # データ範囲を更新
-    st.session_state['x_min_lim'] = min(st.session_state['x_min_lim'], data["time/s"].min())
-    st.session_state['x_max_lim'] = max(st.session_state['x_max_lim'], data["time/s"].max())
-    st.session_state['y_min_lim'] = min(st.session_state['y_min_lim'], data["Ewe/V"].min())
-    st.session_state['y_max_lim'] = max(st.session_state['y_max_lim'], data["Ewe/V"].max())
+    # ラインタイプをサイドバーで選択
+    line_choice = st.sidebar.selectbox(f'フォルダ {i+1} のラインタイプを選択', list(preset_types.keys()), index=0, key=f'line_choice_{i}')
+    st.session_state['line_types'][i] = preset_types[line_choice]
 
-    st.sidebar.divider()
+    # ライン幅をサイドバーで選択
+    line_width = st.sidebar.number_input(f'フォルダ {i+1} のライン幅を入力', min_value=1, max_value=10, value=2, key=f'line_width_{i}')
+    st.session_state['line_widths'][i] = line_width
 
-# 軸ラベルと範囲の設定
-xlabel = st.text_input('x軸のラベルを入力', r'time/s^{1/2}')
-ylabel = st.text_input('y軸のラベルを入力', r'E_{we}/V')
-x_min = st.number_input('x軸の最小値を入力', value=st.session_state['x_min_lim'])
-x_max = st.number_input('x軸の最大値を入力', value=st.session_state['x_max_lim'])
-y_min = st.number_input('y軸の最小値を入力', value=st.session_state['y_min_lim'])
-y_max = st.number_input('y軸の最大値を入力', value=st.session_state['y_max_lim'])
+    # 凡例をサイドバーで入力
+    legend_text = st.sidebar.text_input(f'フォルダ {i+1} の凡例を入力', value=st.session_state['legends'][i], key=f'legend_{i}')
+    st.session_state['legends'][i] = legend_text
 
-# st.session_state['data']にデータがある場合、データをプロット
-for i, data in enumerate(st.session_state['data_folders']):
-    ax.plot(st.session_state['data'][i]["time/s"], st.session_state['data'][i]["Ewe/V"], color=st.session_state['plot_settings'][i]['color'], linestyle=st.session_state['line'][i]['line_type'], linewidth=st.session_state['linewidth'][i]['line_width'])
+# プロットタイプがCPの場合のみプロットを表示
+if plot_type == 'CP':
+    # 軸ラベルと範囲の設定
+    xlabel = st.text_input('x軸のラベルを入力', 'time/s')
+    ylabel = st.text_input('y軸のラベルを入力', 'Ewe/V')
+    x_min = st.number_input('x軸の最小値を入力', value=st.session_state['x_min'])
+    x_max = st.number_input('x軸の最大値を入力', value=st.session_state['x_max'])
+    y_min = st.number_input('y軸の最小値を入力', value=st.session_state['y_min'])
+    y_max = st.number_input('y軸の最大値を入力', value=st.session_state['y_max'])
 
-# グラフの設定を適用
-ax.set_xlabel(f'${xlabel}$')
-ax.set_ylabel(f'${ylabel}$')
-ax.set_xlim([x_min, x_max])
-ax.set_ylim([y_min, y_max])
-ax.invert_yaxis()
-st.pyplot(fig)
+    # Plotlyモードの場合のプロット
+    if plot_mode == 'plotly':
+        # グラフのプロット
+        fig = go.Figure()
+
+        # st.session_state['data']にデータがある場合、データをプロット
+        for i, data in enumerate(st.session_state['data']):
+            fig.add_trace(go.Scatter(
+                x=data["time/s"], 
+                y=data["Ewe/V"],
+                mode='lines',
+                line=dict(color=st.session_state['plot_colors'][i], dash=st.session_state['line_types'][i], width=st.session_state['line_widths'][i]),
+                name=st.session_state['legends'][i]
+            ))
+
+        # グラフの設定を適用
+        fig.update_layout(
+            xaxis_title=xlabel,
+            yaxis_title=ylabel,
+            xaxis=dict(range=[x_min, x_max]),
+            yaxis=dict(range=[y_min, y_max]),
+            yaxis_autorange='reversed',  # y軸を反転
+            plot_bgcolor='black',  # 背景色を黒に設定
+            paper_bgcolor='black'  # 背景色を黒に設定
+        )
+
+        st.plotly_chart(fig)
+
+    # Matplotlibモードの場合のプロット
+    elif plot_mode == 'matplot':
+        fig, ax = plt.subplots()
+
+        # st.session_state['data']にデータがある場合、データをプロット
+        for i, data in enumerate(st.session_state['data']):
+            ax.plot(
+                data["time/s"], 
+                data["Ewe/V"], 
+                color=st.session_state['plot_colors'][i], 
+                linestyle=matplotlib_line_types[st.session_state['line_types'][i]], 
+                linewidth=st.session_state['line_widths'][i],
+                label=st.session_state['legends'][i]
+            )
+
+        # 軸ラベルと範囲の設定
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_xlim([x_min, x_max])
+        ax.set_ylim([y_min, y_max])
+        ax.invert_yaxis()  # y軸を反転
+        ax.legend()
+
+        st.pyplot(fig)
+
+        # グラフを保存するボタン
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        st.download_button(
+            label="グラフを保存",
+            data=buf,
+            file_name="plot.png",
+            mime="image/png"
+        )
 
 if not st.session_state['data_folders']:
     st.write('データを追加してください。')
--- run_app_test.py
+++ run_app_test.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pandas as pd
 import os
 from data_obtain import CP_grapher
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+from io import BytesIO
 
 # タイトル
-st.title('複数データを管理するシンプルなアプリ')
+st.title('複数データを管理するアプリ')
 
 # セッションステートの初期化
 if 'data_folders' not in st.session_state:
@@ -18,6 +20,8 @@ if 'legends' not in st.session_state:
     st.session_state['legends'] = []
 if 'line_types' not in st.session_state:
     st.session_state['line_types'] = []
+if 'line_widths' not in st.session_state:
+    st.session_state['line_widths'] = []
 if 'x_min' not in st.session_state:
     st.session_state['x_min'] = 0.0
 if 'x_max' not in st.session_state:
@@ -26,6 +30,9 @@ if 'y_min' not in st.session_state:
     st.session_state['y_min'] = 0.0
 if 'y_max' not in st.session_state:
     st.session_state['y_max'] = 10.0
+
+# サイドバーにプロットモード選択ウィジェットを追加
+plot_mode = st.sidebar.selectbox('プロットモードを選択', ['matplot', 'plotly'])
 
 # サイドバーにCV / CPの選択欄を追加
 plot_type = st.sidebar.selectbox('プロットタイプを選択', ['CP', 'CV'])
@@ -42,6 +49,7 @@ if st.sidebar.button('フォルダを追加'):
         st.session_state['plot_colors'].append('white')  # デフォルトの色を白に設定
         st.session_state['legends'].append(os.path.basename(folder))  # デフォルトの凡例をフォルダ名に設定
         st.session_state['line_types'].append('solid')  # デフォルトの線の種類を設定
+        st.session_state['line_widths'].append(2)  # デフォルトの線幅を設定
         # 各軸の最小値と最大値を計算
         st.session_state['x_min'] = min(st.session_state['x_min'], data["time/s"].min())
         st.session_state['x_max'] = max(st.session_state['x_max'], data["time/s"].max())
@@ -59,6 +67,12 @@ preset_types = {
     "dashdot": 'dashdot',
     "dot": 'dot'
 }
+matplotlib_line_types = {
+    "solid": '-',
+    "dash": '--',
+    "dashdot": '-.',
+    "dot": ':'
+}
 
 for i, folder in enumerate(st.session_state['data_folders']):
     st.sidebar.write(f'フォルダ {i+1}: {os.path.basename(folder)}')
@@ -70,6 +84,10 @@ for i, folder in enumerate(st.session_state['data_folders']):
     # ラインタイプをサイドバーで選択
     line_choice = st.sidebar.selectbox(f'フォルダ {i+1} のラインタイプを選択', list(preset_types.keys()), index=0, key=f'line_choice_{i}')
     st.session_state['line_types'][i] = preset_types[line_choice]
+
+    # ライン幅をサイドバーで選択
+    line_width = st.sidebar.number_input(f'フォルダ {i+1} のライン幅を入力', min_value=1, max_value=10, value=2, key=f'line_width_{i}')
+    st.session_state['line_widths'][i] = line_width
 
     # 凡例をサイドバーで入力
     legend_text = st.sidebar.text_input(f'フォルダ {i+1} の凡例を入力', value=st.session_state['legends'][i], key=f'legend_{i}')
@@ -85,31 +103,68 @@ if plot_type == 'CP':
     y_min = st.number_input('y軸の最小値を入力', value=st.session_state['y_min'])
     y_max = st.number_input('y軸の最大値を入力', value=st.session_state['y_max'])
 
-    # グラフのプロット
-    fig = go.Figure()
+    # Plotlyモードの場合のプロット
+    if plot_mode == 'plotly':
+        # グラフのプロット
+        fig = go.Figure()
 
-    # st.session_state['data']にデータがある場合、データをプロット
-    for i, data in enumerate(st.session_state['data']):
-        fig.add_trace(go.Scatter(
-            x=data["time/s"], 
-            y=data["Ewe/V"],
-            mode='lines',
-            line=dict(color=st.session_state['plot_colors'][i], dash=st.session_state['line_types'][i]),
-            name=st.session_state['legends'][i]
-        ))
+        # st.session_state['data']にデータがある場合、データをプロット
+        for i, data in enumerate(st.session_state['data']):
+            fig.add_trace(go.Scatter(
+                x=data["time/s"], 
+                y=data["Ewe/V"],
+                mode='lines',
+                line=dict(color=st.session_state['plot_colors'][i], dash=st.session_state['line_types'][i], width=st.session_state['line_widths'][i]),
+                name=st.session_state['legends'][i]
+            ))
 
-    # グラフの設定を適用
-    fig.update_layout(
-        xaxis_title=xlabel,
-        yaxis_title=ylabel,
-        xaxis=dict(range=[x_min, x_max]),
-        yaxis=dict(range=[y_min, y_max]),
-        yaxis_autorange='reversed',  # y軸を反転
-        plot_bgcolor='black',  # 背景色を黒に設定
-        paper_bgcolor='black'  # 背景色を黒に設定
-    )
+        # グラフの設定を適用
+        fig.update_layout(
+            xaxis_title=xlabel,
+            yaxis_title=ylabel,
+            xaxis=dict(range=[x_min, x_max]),
+            yaxis=dict(range=[y_min, y_max]),
+            yaxis_autorange='reversed',  # y軸を反転
+            plot_bgcolor='black',  # 背景色を黒に設定
+            paper_bgcolor='black'  # 背景色を黒に設定
+        )
 
-    st.plotly_chart(fig)
+        st.plotly_chart(fig)
+
+    # Matplotlibモードの場合のプロット
+    elif plot_mode == 'matplot':
+        fig, ax = plt.subplots()
+
+        # st.session_state['data']にデータがある場合、データをプロット
+        for i, data in enumerate(st.session_state['data']):
+            ax.plot(
+                data["time/s"], 
+                data["Ewe/V"], 
+                color=st.session_state['plot_colors'][i], 
+                linestyle=matplotlib_line_types[st.session_state['line_types'][i]], 
+                linewidth=st.session_state['line_widths'][i],
+                label=st.session_state['legends'][i]
+            )
+
+        # 軸ラベルと範囲の設定
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_xlim([x_min, x_max])
+        ax.set_ylim([y_min, y_max])
+        ax.invert_yaxis()  # y軸を反転
+        ax.legend()
+
+        st.pyplot(fig)
+
+        # グラフを保存するボタン
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        st.download_button(
+            label="グラフを保存",
+            data=buf,
+            file_name="plot.png",
+            mime="image/png"
+        )
 
 if not st.session_state['data_folders']:
     st.write('データを追加してください。')
